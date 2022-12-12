504b4fbb7fefedfe12325dc83f98d4b4e36f55a6
Luke Kennedy <lukemkenne@gmail.com>
2022-12-11 23:07:37 -0500

    Added more detailed comments and updated README slightly (again)

--- README.txt
+++ README.txt
@@ -7,7 +7,8 @@ A multiplier is added for larger communities, as a larger community generally me
 this app can be used to discover Ethereum-based NFT collections with the most well-resourced NFT collectors. The application uses JavaScript on the client-side and Python 
 with Flask on the server-side to accomplish this task.
 
-NOTE: Recently, the Moralis API has been experiencing downtime. This may be a cause for any 500 Errors experienced. Check API uptime on https://status.moralis.io/.
+NOTE: To use this app on your local machine, you will need a API key from https://moralis.io/nft-api/. Place this in a 'config.py' file, set to a variable called MORALIS_API_KEY.
+Recently, the Moralis API has been experiencing downtime. This may be a cause for any 500 Errors experienced. Check API uptime on https://status.moralis.io/.
 This app currently runs in Python, so there is no parallel processing involved. In order to compute the score of a collection, one API call 
 must be made per wallet to retrieve native token balance. As a result, processing of large collections can take a long time. Re-architecting this 
 application using a language that supports multi-threading (such as Go) is currently of high priority. For more information during the score computation process 
@@ -15,15 +16,14 @@ in these development stages, please refer to the console after clicking 'Submit'
 Pixel Vault Core NFT Collection: 0xFb10b1717C92e9cc2d634080c3c337808408D9E1
 
 TODO:
-- Parallel processing for the wallet API calls (potential re-architecture to background processing. Potentially a change in language (GO?))
+- Parallel processing for the wallet API calls (potential re-architecture to background processing. Potentially a change in language (Go?))
 - Increase error-handling capabilities
-- Add a loading animation since it usually takes a few seconds to compute a score.
+- Add a loading animation since it takes time to compute a score.
 - Implement a cached "rankings" list of pre-computed collection scores that can be displayed upon page-load. This will
   allow for more context when generating scores. New rankings may be computed and re-cached once per 24h.
 - Add logo, banner at the top of site, and other aesthetic touches.
 - QoL: Include "history" where, when computing a collection, instead of replacing a already computed value with a new value, just bump the old value down.
 - QoL: Capture name of collection and instead of writing "Collection Score: ..." have a fully formed response with Collection Name, Contract address, 
   collection size, # holders, and score.
-- Add some more optional analytics that can be requested (i.e. "this collection is in the Xth percentile", scatter plots using matplotlib (maybe), etc.)
-- Implement React (may use PyReact to stay consistent w/ server-side)
-
+- Add some more optional analytics that can be requested (i.e. "this collection is in the Xth percentile", scatter plots using matplotlib or an equivalent (maybe), etc.)
+- Implement React
--- index.py
+++ index.py
@@ -2,11 +2,10 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS
 from moralis import evm_api
 from config import MORALIS_API_KEY  # Secure import from config.py
-import logging
 # import sys
 # import logging
 
-# configure logging settings
+# logging settings
 # logging.basicConfig(level=logging.DEBUG, stream=sys.stdout)
 
 # Ghxsts: 0xcBd38d10511F0274e040085c0BC1F85CC96Fff82
@@ -16,21 +15,21 @@ import logging
 # Flow of process:
 # 1.) Input collection contract address
 # 2.) Get REST response and filter it using the requests module
-# 3.) Create an array of all of the wallets that own a NFT from specified collection
-# 4.) Take that array and loop through it, passing an address into the findBalance() function for each iteration
-# 5.) Build another array of floats (ETH balances)
-# 6.) Average this array (or otherwise create some sort of score based on holders:avg liquidity that indicates how liquid collectors are on average)
-# 7.) Display this number on the screen.
-# 8.) Include a note that outlines what will be a part of v2. Listing out collections on a LB, collection photos,
-#     and if there is no score based on holders:avg liq, then that. Also, if you dont get to it, add REACT and replace the CSS.
-# Include "the average score of the top 25 blue-chip collections is "xyz". Read our README to learn how we are making score contextualization easier
-# in the coming updates.
+# 3.) Create de-duplicated set of all of the wallets that own a NFT from specified collection
+# 4.) Take that set and loop through it, passing an address into the findBalance() function for each iteration
+# 5.) Build a list of floats (ETH balances)
+# 6.) Compute score of this list using score()
+# 7.) Display this number as output.
 
 
+# This function is the one that drives the whole computation process. It's goal is to take the input of a NFT contract
+# address, filter the API response for just wallets, pass a de-duplicated set of wallets into findBalance(), then
+# compute score in the return statement.
 def getHolders(contractAddress):
 
     walletList = []
 
+    # list of parameters that the API takes
     params = {
         "address": contractAddress,
         "chain": "eth",
@@ -40,6 +39,8 @@ def getHolders(contractAddress):
         "normalizeMetadata": True,
     }
 
+    # Create an infinite while loop to paginate API requests.
+    # Break when the "cursor" JWT (returned by API to denote page #) == None. This means no more response pages exist.
     while True:
         data = evm_api.nft.get_nft_owners(
             api_key=MORALIS_API_KEY,
@@ -59,23 +60,27 @@ def getHolders(contractAddress):
     print("Pre-deduplicated Wallets: ", len(walletList))
     # logging.info("STILL GOOD ON LINE 66")
 
-    # De-duplicate the list
+    # De-duplicate the list by passing it to a set
     walletSet = set(walletList)
 
     # logging.info("STILL GOOD on line 70")
 
+    # Create a list full of Ether values of holder wallets. findBalance() will return
+    # key:value pairs that must first be added to a dictionary, then the values will be extracted to a list.
     balanceDict = []
     for wallet in walletSet:
         balanceDict.append(findBalance(wallet))
 
     balanceList = []
     for balances in balanceDict:
+        # Divide by 1E18 because Moralis returns response w/ decimals included.
+        # i.e. 9.12 ETH would be returned as something like 912051407680925461166
         balanceList.append(float(balances["balance"]) / 1E18)
 
     return "Collection score: " + str(score(balanceList))
 
 
-# Returns the Ether balance of a wallet using Moralis get_native_balance()
+# Takes in a wallet address and returns the Ether balance of a wallet using Moralis endpoint get_native_balance()
 def findBalance(walletAddress):
     params = {
         "address": walletAddress,
@@ -90,7 +95,8 @@ def findBalance(walletAddress):
     return result
 
 
-# Scoring function
+# Scoring function that takes the average divided by the standard deviation of all values.
+# A multiplier is then added to account for collection size (theoretically, larger collection = more active members)
 def score(accounts):
     total_value = sum(accounts)
     avg_value = total_value / len(accounts)
@@ -107,21 +113,19 @@ def score(accounts):
     finalScore = (avg_value / stdev) + multiplier
 
     print("Collection Score: ", finalScore)
+
     return finalScore
 
 
-# Testing Area
-# getHolders("0x6AF88A2B51D37DD80B59CdA3aaac55eE34C6ea07")
+# Flask app that takes user input from index.html
 app = Flask(__name__)
 CORS(app)
-
-# Update to "Calculate Score" (also update below line 123)
 
 
 @ app.route('/calculate_score', methods=['POST'])
 def calculate_score():
-    # Get the input value from the form submission these variables must match what was passed through
-    # on the client side, otherwise you will get a 400 error
+    # Get the input value from the form submission. These variables must match what was passed through
+    # on the client side, otherwise you will get a 400 error.
     data = request.get_json()
     inputValue = data['input_field']
 
